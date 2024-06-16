3c3021f6df4b50668fd3d00c4f958c87cf9aa528
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-06-16 10:37:31 -0300

    html finishd, elements called

---
+++ script.js
@@ -0,0 +1,6 @@
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateYear = document.getElementById('dateYear');
+const dateMonth = document.getElementById('dateMonth');
+
+const taskContainer = document.getElementById('taskContainer');
